8648f35e699d8247e6cacb731880691996fe09e3
root <emmaolondo448@gmail.com>
2023-10-11 07:37:07 -0700

    *args, **kwargs arguments for the constructor of a BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -8,16 +8,24 @@ import uuid
 class BaseModel:
     """ The parent class ofvthe AIRBNB project
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ initialization of class attributes
         args:
             id - string - assign with an uuid when an instance is created
             created_at - datetime - shows when an instance was created
             updated_at - datetime - updates an instance created
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """prints the string representation of the objects
@@ -32,7 +40,7 @@ class BaseModel:
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        return self.save()
+        """return self.save()"""
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__
@@ -46,5 +54,5 @@ class BaseModel:
             new_dict["created_at"] = self.created_at.isoformat()
         if "updated_at" in new_dict:
             new_dict["updated_at"] = self.updated_at.isoformat()
-        new_dict["__class__"] = self.__class__.name
+        new_dict["__class__"] = self.__class__.__name__
         return new_dict
