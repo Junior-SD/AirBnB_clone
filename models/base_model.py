8c8a1f9283bc79fe46236ff6ce4dcb6dabe73ae0
root <emmaolondo448@gmail.com>
2023-10-11 04:26:09 -0700

    dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -6,14 +6,14 @@ import uuid
 
 
 class BaseModel:
-    """ The parent class ofvthe AIRBNB project that puts in place initialization, serialization and deserialization
+    """ The parent class ofvthe AIRBNB project
     """
     def __init__(self):
         """ initialization of class attributes
         args:
             id - string - assign with an uuid when an instance is created
-            created_at - datetime - assign with the current datetime when an instance is created
-            updated_at - datetime - assign with the current datetime when an instance is created and it will be updated every time you change your object
+            created_at - datetime - shows when an instance was created
+            updated_at - datetime - updates an instance created
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
@@ -23,18 +23,28 @@ class BaseModel:
         """prints the string representation of the objects
         print class name, id, __dict__
         """
-        return(f"[{self.__class__.__name__}] ({self.id}) {self.__class__.__dict__}")
-       
+        c = self.__class__.__name__
+        dic = self.__class__.__dict__
+        return(f"[{c}] ({self.id}) {dic}")
+
     def save(self):
-        """ updates the public instance attribute updated_at with the current datetime
+        """ updates the public instance attribute
+        updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        save()
+        return self.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance:
+        """returns a dictionary containing all keys/values of __dict__
         by using:
             self.__dict__, only instance attributes set will be returned
-            a key __class__ must be added to this dictionary with the class name of the object
+            a key __class__ must be added to this dictionary with
+            the class name of the object
         """
-        """"###Not sure how to implement it ####"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = self.created_at.isoformat()
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.name
+        return new_dict
